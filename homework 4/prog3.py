e0df9e3e90553cc9763237a57685537c84759696
HeavMas <94596205+HeavMas@users.noreply.github.com>
2022-12-19 20:49:06 +0300

    Add files via upload

---
+++ homework 4/prog3.py
@@ -0,0 +1,15 @@
+def F(n):
+    if n <= 2:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+    else:
+        arr = [1] * (n + 1)
+        arr[0] = 0
+        arr[2] = 1
+        for i in range(3, n + 1):
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
+    return arr[n]
+
+print(F(3))
