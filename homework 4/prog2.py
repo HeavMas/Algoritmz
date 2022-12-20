cfb9993452e24a8479f223376313bbc7dafadeec
HeavMas <94596205+HeavMas@users.noreply.github.com>
2022-12-20 13:31:14 +0300

    Update prog2.py

--- homework 4/prog2.py
+++ homework 4/prog2.py
@@ -1,18 +1,18 @@
 def F(root):
-    stack = [(root, 0)]
-    lst = []
-    while stack:
-        value, leng = stack.pop()
-        if value.left != None:
-            lst.append((value.left).val)
-            stack.append((value.left, leng + 1))
-        if value.right != None:
-            lst.append((value.right).val)
-            stack.append((value.right, leng + 1))
-    min_v = 10 ** 6
-    lst.append(root.val)
-    lst.sort()
-    for i in range(len(lst)):
-        if min_v > abs(lst[i - 1] - lst[i]):
-            min_v = abs(lst[i - 1] - lst[i])
-    return min_v
+        stack = [root]
+        lst = []
+        while stack:
+            ptr = stack.pop()
+            if ptr.left:
+                stack.append(ptr.left)
+                lst.append((ptr.left).val)
+            if ptr.right:
+                stack.append(ptr.right)
+                lst.append((ptr.right).val)
+        min_v = 10 ** 6
+        lst.append(root.val)
+        lst.sort()
+        for i in range(len(lst)):
+            if min_v > abs(lst[i - 1] - lst[i]):
+                min_v = abs(lst[i - 1] - lst[i])
+        return min_v
